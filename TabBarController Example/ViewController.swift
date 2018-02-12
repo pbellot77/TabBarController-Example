ebb8bf58116f5a348afe9060eba46a5fd73f192f
Patrick <patrickbellot77@gmail.com>
2018-02-12 10:13:43 -0600

    Initial Commit

---
+++ TabBarController Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarController Example
+//
+//  Created by Patrick Bellot on 2/12/18.
+//  Copyright © 2018 Polestar Interactive LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
